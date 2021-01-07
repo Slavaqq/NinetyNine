7decc0f05c90aedee541a5131649adb234b9136e
Slava <slavaqq@gmail.com>
2021-01-07 18:59:54 +0100

    third problem solution without helper function

--- problems/problem3.hs
+++ problems/problem3.hs
@@ -1,9 +1,7 @@
 -- Find the K'th element of a list. The first element in the list is number 1.
 
 elementAt :: [a] -> Integer -> Maybe a
-elementAt list position
+elementAt (x : xs) position
     | position < 1 = Nothing
-    | otherwise = go list position 1 
-    where
-         go [] _ _ = Nothing
-         go (x : xs) at i = if at == i then Just x else go xs at (i + 1)
+    | position == 1 = Just x
+    | otherwise = elementAt xs $ position - 1
