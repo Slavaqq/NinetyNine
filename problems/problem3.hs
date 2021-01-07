da4c91aa56c11b9072afaba7b111bbc621c44a8f
Slava <slavaqq@gmail.com>
2021-01-07 19:06:13 +0100

    third problem solution for an empty list

--- problems/problem3.hs
+++ problems/problem3.hs
@@ -1,6 +1,7 @@
 -- Find the K'th element of a list. The first element in the list is number 1.
 
 elementAt :: [a] -> Integer -> Maybe a
+elementAt [] _ = Nothing
 elementAt (x : xs) position
     | position < 1 = Nothing
     | position == 1 = Just x
