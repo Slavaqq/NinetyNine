1338451c85c3db3ac50e900e9594da0d98b5caff
Slava <slavaqq@gmail.com>
2021-01-11 20:05:54 +0100

    solution for the seventh problem

---
+++ problems/problem7.hs
@@ -0,0 +1,9 @@
+-- Flatten a nested list structure.
+
+data NestedList a = Elem a | List [NestedList a] deriving Show
+
+flatten :: NestedList a -> [a]
+flatten (Elem x) = [x]
+flatten (List xs) = concatMap flatten xs
+
+
